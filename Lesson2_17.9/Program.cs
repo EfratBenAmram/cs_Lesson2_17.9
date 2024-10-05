89b4302cc5c71cceba8a32f2e416eb9e25c72108
EfratBenAmram <efrat.benamram1@gmail.com>
2024-10-05 21:40:02 +0300

    Add files via upload

---
+++ Lesson2_17.9/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson1_10._9
+{
+    internal class Program
+    {
+        public static void Students10_9()
+        {
+            /*
+            Student s = new Student("Efrat", 20, "123456789", "chipet");
+            s.AddCourse("c");
+            s.AddCourse("c++");
+            s.AddCourse("c#");
+            Console.WriteLine("Details of the new student:");
+            s.PrintDetails();
+            s.ShowCourses();*/
+            List<Student> students = new List<Student> {
+                new Student("shira", 19, "67845634", "yu"),
+                new Student("dina", 35, "67845634", "uy"),
+                new Student("leah", 24, "67847634", "re"),
+                new Student("sara", 15, "67845634", "gh")
+            };
+
+            Console.WriteLine("Students:");
+            foreach (var stu in students)
+                stu.PrintDetails();
+            Console.WriteLine();
+
+            var upTo20 = students.Where(x => x.Age > 20);
+            var sBegin = students.Where(ns => ns.Name.StartsWith("s"));
+
+            Console.WriteLine("Students older than 20:");
+            foreach (var stu in upTo20)
+                stu.PrintDetails();
+            Console.WriteLine();
+
+            
+            Console.WriteLine("Students whose names start with 's':");
+            foreach (var stu in sBegin)
+                stu.PrintDetails(); 
+            Console.WriteLine();
+        }
+
+        public static void Students17_9()
+        {
+            /* 1.2.
+            Student s1 = new Student("Efrat", 20, "123456789", "chipset");
+            s1.AddCourse();
+            s1.PrintDetails();
+            */
+
+            /* 3. 4. */
+            List<Student> students = new List<Student> {
+                new Student("shira", 19, "67845634", "yu"),
+                new Student("dina", 35, "67845634", "uy"),
+                new Student("leah", 24, "67847634", "re")
+            };
+            foreach (Student stu in students)
+            {
+                Console.WriteLine("for the dear student " + stu.Name);
+                stu.AddCourse();
+                Console.WriteLine("\n");
+            }
+            ShowStudents(students);
+
+            var sBegin = students.Where(ns => ns.Name.StartsWith("s")).ToList();
+            var studentsInGroup = students.Where(stu => stu.Group == "yu").ToList();
+            var studentsWithMoreThanOneCourse = students.Where(stu => stu.Courses.Count > 1).ToList();
+            var studentsWithLowScores = students.Where(stu => stu.Scores.Values.Any(score => score < 56)).ToList();
+
+            Console.WriteLine("Students whose names start with 's':");
+            ShowStudents(sBegin);
+
+            Console.WriteLine("Students in the group 'yu':");
+            ShowStudents(studentsInGroup);
+
+            Console.WriteLine("Students enrolled in more than one course:");
+            ShowStudents(studentsWithMoreThanOneCourse);
+
+            Console.WriteLine("Students with a score less than 56:");
+            ShowStudents(studentsWithLowScores);
+
+            Console.ReadLine();
+        }
+
+        public static void ShowStudents(List<Student> students)
+        {
+            foreach (Student stu in students)
+            {
+                stu.PrintDetails();
+                Console.WriteLine("\n");
+            }
+        }
+
+        static void Main(string[] args)
+            {
+            /*
+             * 10.9
+             * Students10_9();
+             */
+
+            /* 
+             *  17.9
+             */  Students17_9();
+             
+
+        }
+    }
+}
---
+++ Lesson2_17.9/Student.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson1_10._9
+{
+    internal class Student
+    {
+        private string name;
+        private int age;
+        private string id;
+        private string group;
+        //private int cntCourses;
+        private List<string> courses;
+        private Dictionary<string, int> scores;
+
+        public Student(string name, int age, string id, string group)
+        {
+            Name = name;
+            Group = group;
+            Age = age;
+            Id = id;
+            //cntCourses = 0;
+            Courses = new List<string>();
+            Scores = new Dictionary<string, int>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 18)
+                    age = value;
+            }
+        }
+
+        public string Id
+        {
+            get { return id; }
+            set
+            {
+                if (value.Length == 9)
+                    id = value;
+            }
+        }
+
+        public string Group
+        {
+            get { return group; }
+            set { group = value; }
+        }
+
+        public List<string> Courses
+        {
+            get { return courses; }
+            set { courses = value; }
+        }
+
+        public Dictionary<string, int> Scores
+        {
+            get { return scores; }
+            set { scores = value; }
+        }
+
+        /*
+        public void AddCourse(string course)
+        {
+            if (cntCourses < 5)
+                courses[cntCourses++] = course;
+            else
+                Console.WriteLine("Courses are full");
+        }
+        */
+        public void AddCourse()
+        {
+            Console.WriteLine("enter the course and the grade:");
+            string course = Console.ReadLine();
+            int grade = Int32.Parse(Console.ReadLine());
+            while (course != "0") { 
+                Courses.Add(course);
+                Scores.Add(course, grade);
+                Console.WriteLine("enter the course and the grade:");
+                course = Console.ReadLine();
+                grade = Int32.Parse(Console.ReadLine());
+            }
+        }
+
+        public void ShowCourses()
+        {
+            Console.WriteLine("The courses:");
+            foreach (string course in Courses)
+                if (course != null)
+                    Console.WriteLine(course);
+        }
+
+        public void ShowScores()
+        {
+            Console.WriteLine("The scores:");
+            foreach (var score in Scores)
+            {
+                Console.WriteLine("\n\tIn course: " + score.Key);
+                Console.WriteLine("\tThe score: " + score.Value);
+            }
+        }
+        /*
+         public void ShowCourses()
+        {
+            Console.WriteLine("The courses:");
+            for (int i=0; i<cntCourses; i++)
+                if (courses[i] != null)
+                    Console.WriteLine(courses[i]);
+        }
+         */
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Name: Miss. {Name}, Age: {Age}, ID: {Id}, Group: {Group}");
+            ShowScores();
+        }
+    }
+}
